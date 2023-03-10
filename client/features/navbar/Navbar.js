25615cc210d8b64e4c23e1e4e4fffb271022a15a
Zelda Ogiamien <zel0789@yahoo.com>
2023-03-10 18:58:13 -0500

    updated navbar links

--- client/features/navbar/Navbar.js
+++ client/features/navbar/Navbar.js
@@ -20,6 +20,8 @@ const Navbar = () => {
           <div>
             {/* The navbar will show these links after you log in */}
             <Link to="/home">Home</Link>
+            <Link to="/profile">Profile</Link>
+            <Link to="/users">Users</Link>
             <button type="button" onClick={logoutAndRedirectHome}>
               Logout
             </button>
@@ -29,7 +31,7 @@ const Navbar = () => {
             {/* The navbar will show these links before you log in */}
             <Link to="/login">Login</Link>
             <Link to="/signup">Sign Up</Link>
-            <Link to="/users">Users</Link>
+            {/* <Link to="/users">Users</Link> */}
           </div>
         )}
       </nav>
